03975dc2f544e4df5e320e1f538d73e3ac0a4066
Michael-J-Conte <mconte2@binghamton.edu>
2017-12-28 22:50:03 -0500

    removed function abstraction to increase compution speed and updated device name to match udev rules

--- src/xboxdrivertest.py
+++ src/xboxdrivertest.py
@@ -2,34 +2,22 @@
 
 import evdev 
 
-def controller_input(port):
-    device = evdev.InputDevice(port)#"/dev/input/event0")
-    for event in device.read_loop():
-        if event.type == 3: #check to see if event type is an absolute axis event can also use evdev.ecodes.EV_ABS instead of 3
-            if event.code == 3: 
-                return [event.value, 'right']
-            elif event.code == 0:
-                return [event.value, 'left']
-        
-       # if event.code == 4 and event.value > 0:
-       #     print("-x")
-       # elif event.code == 4 and event.value < 0:
-       #     print("x")
+device = evdev.InputDevice("/dev/xbox1")#"/dev/input/event0")
+for event in device.read_loop():
+    if event.type == 3: #check to see if event type is an absolute axis event can also use evdev.ecodes.EV_ABS instead of 3
+        if event.code == 3: 
+            print([event.value, 'right'])
+            
+        elif event.code == 0:
+            print([event.value, 'left'])
 
-       # elif event.code == 3 and event.value > 0:
-       #     print("-y")
-       # elif event.code == 3 and event.value < 0:
-       #     print("y")
-       # print(evdev.categorize(event))
-       # print(repr(event))
-       # print(event)
 
-def main():
-    while(True):
-        output = controller_input("/dev/input/event0")
-        print(output)
 
-main()
+
+
+
+
+### changes graveyard ###
 
 
 #from evdev import InputDevice, categorize, ecodes
@@ -43,3 +31,16 @@ main()
 #        print(categorize(event))
 
 
+        
+       # if event.code == 4 and event.value > 0:
+       #     print("-x")
+       # elif event.code == 4 and event.value < 0:
+       #     print("x")
+
+       # elif event.code == 3 and event.value > 0:
+       #     print("-y")
+       # elif event.code == 3 and event.value < 0:
+       #     print("y")
+       # print(evdev.categorize(event))
+       # print(repr(event))
+       # print(event)
