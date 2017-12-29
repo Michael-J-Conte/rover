1a98aac8377668f92c5b827675afefaa06e974f9
Michael-J-Conte <mconte2@binghamton.edu>
2017-12-28 19:58:16 -0500

    abstracted reading the controller input into a function and created a main function

--- src/xboxdrivertest.py
+++ src/xboxdrivertest.py
@@ -2,26 +2,34 @@
 
 import evdev 
 
-device = evdev.InputDevice("/dev/input/event0")
-for event in device.read_loop():
-    if event.type == evdev.ecodes.EV_ABS:
-        if event.code == 4 and event.value > 0:
-            print("x")
-        elif event.code == 4 and event.value < 0:
-            print("-x")
+def controller_input(port):
+    device = evdev.InputDevice(port)#"/dev/input/event0")
+    for event in device.read_loop():
+        if event.type == 3: #check to see if event type is an absolute axis event can also use evdev.ecodes.EV_ABS instead of 3
+            if event.code == 3: 
+                return [event.value, 'right']
+            elif event.code == 0:
+                return [event.value, 'left']
+        
+       # if event.code == 4 and event.value > 0:
+       #     print("-x")
+       # elif event.code == 4 and event.value < 0:
+       #     print("x")
 
-        elif event.code == 3 and event.value > 0:
-            print("y")
-        elif event.code == 3 and event.value < 0:
-            print("-y")
+       # elif event.code == 3 and event.value > 0:
+       #     print("-y")
+       # elif event.code == 3 and event.value < 0:
+       #     print("y")
+       # print(evdev.categorize(event))
+       # print(repr(event))
+       # print(event)
 
-        #print(evdev.categorize(event))
-        #print(repr(event))
-        #print(event)
+def main():
+    while(True):
+        output = controller_input("/dev/input/event0")
+        print(output)
 
-
-
-
+main()
 
 
 #from evdev import InputDevice, categorize, ecodes
